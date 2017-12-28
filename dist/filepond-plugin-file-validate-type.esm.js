7fb6bd10fa7fca60da7c193380b7635c22ca6986
Rik Schennink <rikschennink@gmail.com>
2017-12-28 09:22:59 +0100

    move build targets to dist folder

---
+++ dist/filepond-plugin-file-validate-type.esm.js
@@ -0,0 +1,126 @@
+/*
+ * FilePondPluginFileValidateType 1.0.0
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+var plugin$1 = ({ addFilter, utils }) => {
+  // get quick reference to Type utils
+  const {
+    Type,
+    isString,
+    guesstimateMimeType,
+    getExtensionFromFilename,
+    getFilenameFromURL
+  } = utils;
+
+  const mimeTypeMatchesWildCard = (mimeType, wildcard) => {
+    const mimeTypeGroup = (/^[^/]+/.exec(mimeType) || []).pop(); // image/png -> image
+    const wildcardGroup = wildcard.slice(0, -2); // image/* -> image
+    return mimeTypeGroup === wildcardGroup;
+  };
+
+  const isValidMIMEType = (acceptedTypes, userInputType) =>
+    acceptedTypes.some(acceptedType => {
+      // accepted is wildcard mime type
+      if (/\*$/.test(acceptedType)) {
+        return mimeTypeMatchesWildCard(userInputType, acceptedType);
+      }
+
+      // is normal mime type
+      return acceptedType === userInputType;
+    });
+
+  const validateFile = (item, acceptedFileTypes) => {
+    // no types defined, everything is allowed \o/
+    if (acceptedFileTypes.length === 0) {
+      return true;
+    }
+
+    // if the item is a url we guess the mime type by the extension
+    let type = '';
+    if (isString(item)) {
+      const filename = getFilenameFromURL(item);
+      const extension = getExtensionFromFilename(filename);
+      if (extension) {
+        type = guesstimateMimeType(extension);
+      } else {
+        return true;
+      }
+    } else {
+      type = item.type;
+    }
+
+    // validate file type
+    return isValidMIMEType(acceptedFileTypes, type);
+  };
+
+  // setup attribute mapping for accept
+  addFilter('SET_ATTRIBUTE_TO_OPTION_MAP', map =>
+    Object.assign(map, {
+      accept: 'acceptedFileTypes'
+    })
+  );
+
+  // adds options to options register
+  addFilter('SET_DEFAULT_OPTIONS', options =>
+    Object.assign(options, {
+      // Enable or disable file type validation
+      allowFileTypeValidation: [true, Type.BOOLEAN],
+
+      // What file types to accept
+      acceptedFileTypes: [[], Type.ARRAY],
+      // - must be comma separated
+      // - mime types: image/png, image/jpeg, image/gif
+      // - extensions: .png, .jpg, .jpeg
+      // - wildcards: image/*
+
+      labelFileTypeNotAllowed: ['File is of invalid type', Type.STRING]
+    })
+  );
+
+  // called for each file that is loaded
+  // right before it is set to the item state
+  // should return a promise
+  addFilter(
+    'LOAD_FILE',
+    (file, { query }) =>
+      new Promise((resolve, reject) => {
+        const allowFileTypeValidation = query('GET_ALLOW_FILE_TYPE_VALIDATION');
+        if (!allowFileTypeValidation) {
+          resolve(file);
+          return;
+        }
+
+        const acceptedFileTypes = query('GET_ACCEPTED_FILE_TYPES');
+
+        // if invalid, exit here
+        if (!validateFile(file, acceptedFileTypes)) {
+          reject({
+            status: {
+              main: query('GET_LABEL_FILE_TYPE_NOT_ALLOWED'),
+              sub: query('GET_ACCEPTED_FILE_TYPES').join(', ')
+            }
+          });
+          return;
+        }
+
+        // all fine
+        resolve(file);
+      })
+  );
+
+  // receives all items and returns true or false depending on wether the items are valid
+  // (as in, is hovered over it)
+  // addFilter('ALLOW_HOPPER_ITEM', ( item, { query }) => {
+  //  TODO: implement, plus throw error in dropzone to indicate item is not valid
+  // });
+};
+
+if (document) {
+  // plugin has loaded
+  document.dispatchEvent(
+    new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
+  );
+}
+
+export default plugin$1;
---
+++ dist/filepond-plugin-file-validate-type.js
@@ -0,0 +1,143 @@
+/*
+ * FilePondPluginFileValidateType 1.0.0
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? (module.exports = factory())
+    : typeof define === 'function' && define.amd
+      ? define(factory)
+      : (global.FilePondPluginFileValidateType = factory());
+})(this, function() {
+  'use strict';
+
+  var plugin$1 = function(_ref) {
+    var addFilter = _ref.addFilter,
+      utils = _ref.utils;
+
+    // get quick reference to Type utils
+    var Type = utils.Type,
+      isString = utils.isString,
+      guesstimateMimeType = utils.guesstimateMimeType,
+      getExtensionFromFilename = utils.getExtensionFromFilename,
+      getFilenameFromURL = utils.getFilenameFromURL;
+
+    var mimeTypeMatchesWildCard = function mimeTypeMatchesWildCard(
+      mimeType,
+      wildcard
+    ) {
+      var mimeTypeGroup = (/^[^/]+/.exec(mimeType) || []).pop(); // image/png -> image
+      var wildcardGroup = wildcard.slice(0, -2); // image/* -> image
+      return mimeTypeGroup === wildcardGroup;
+    };
+
+    var isValidMIMEType = function isValidMIMEType(
+      acceptedTypes,
+      userInputType
+    ) {
+      return acceptedTypes.some(function(acceptedType) {
+        // accepted is wildcard mime type
+        if (/\*$/.test(acceptedType)) {
+          return mimeTypeMatchesWildCard(userInputType, acceptedType);
+        }
+
+        // is normal mime type
+        return acceptedType === userInputType;
+      });
+    };
+
+    var validateFile = function validateFile(item, acceptedFileTypes) {
+      // no types defined, everything is allowed \o/
+      if (acceptedFileTypes.length === 0) {
+        return true;
+      }
+
+      // if the item is a url we guess the mime type by the extension
+      var type = '';
+      if (isString(item)) {
+        var filename = getFilenameFromURL(item);
+        var extension = getExtensionFromFilename(filename);
+        if (extension) {
+          type = guesstimateMimeType(extension);
+        } else {
+          return true;
+        }
+      } else {
+        type = item.type;
+      }
+
+      // validate file type
+      return isValidMIMEType(acceptedFileTypes, type);
+    };
+
+    // setup attribute mapping for accept
+    addFilter('SET_ATTRIBUTE_TO_OPTION_MAP', function(map) {
+      return Object.assign(map, {
+        accept: 'acceptedFileTypes'
+      });
+    });
+
+    // adds options to options register
+    addFilter('SET_DEFAULT_OPTIONS', function(options) {
+      return Object.assign(options, {
+        // Enable or disable file type validation
+        allowFileTypeValidation: [true, Type.BOOLEAN],
+
+        // What file types to accept
+        acceptedFileTypes: [[], Type.ARRAY],
+        // - must be comma separated
+        // - mime types: image/png, image/jpeg, image/gif
+        // - extensions: .png, .jpg, .jpeg
+        // - wildcards: image/*
+
+        labelFileTypeNotAllowed: ['File is of invalid type', Type.STRING]
+      });
+    });
+
+    // called for each file that is loaded
+    // right before it is set to the item state
+    // should return a promise
+    addFilter('LOAD_FILE', function(file, _ref2) {
+      var query = _ref2.query;
+      return new Promise(function(resolve, reject) {
+        var allowFileTypeValidation = query('GET_ALLOW_FILE_TYPE_VALIDATION');
+        if (!allowFileTypeValidation) {
+          resolve(file);
+          return;
+        }
+
+        var acceptedFileTypes = query('GET_ACCEPTED_FILE_TYPES');
+
+        // if invalid, exit here
+        if (!validateFile(file, acceptedFileTypes)) {
+          reject({
+            status: {
+              main: query('GET_LABEL_FILE_TYPE_NOT_ALLOWED'),
+              sub: query('GET_ACCEPTED_FILE_TYPES').join(', ')
+            }
+          });
+          return;
+        }
+
+        // all fine
+        resolve(file);
+      });
+    });
+
+    // receives all items and returns true or false depending on wether the items are valid
+    // (as in, is hovered over it)
+    // addFilter('ALLOW_HOPPER_ITEM', ( item, { query }) => {
+    //  TODO: implement, plus throw error in dropzone to indicate item is not valid
+    // });
+  };
+
+  if (document) {
+    // plugin has loaded
+    document.dispatchEvent(
+      new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
+    );
+  }
+
+  return plugin$1;
+});

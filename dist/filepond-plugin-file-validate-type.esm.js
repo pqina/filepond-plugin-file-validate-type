aaa4bbc67b6e5fc4025fa3a55b786b069d9fa0f0
Rik Schennink <rikschennink@gmail.com>
2019-02-05 08:59:05 +0100

    adjust peerDependency to match FilePond 4.0.0

--- dist/filepond-plugin-file-validate-type.esm.js
+++ dist/filepond-plugin-file-validate-type.esm.js
@@ -1,5 +1,5 @@
 /*
- * FilePondPluginFileValidateType 1.2.1
+ * FilePondPluginFileValidateType 1.2.2
  * Licensed under MIT, https://opensource.org/licenses/MIT
  * Please visit https://pqina.nl/filepond for details.
  */
@@ -203,7 +203,7 @@ var plugin$1 = ({ addFilter, utils }) => {
 const isBrowser =
   typeof window !== 'undefined' && typeof window.document !== 'undefined';
 
-if (isBrowser && document) {
+if (isBrowser) {
   document.dispatchEvent(
     new CustomEvent('FilePond:pluginloaded', { detail: plugin$1 })
   );
